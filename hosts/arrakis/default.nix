de295c919e2767170ce9815092b46d34da85c75f
Giannin <contact@giannin.ch>
2024-03-16 15:26:33 +0100

    Added first host

---
+++ hosts/arrakis/default.nix
@@ -0,0 +1,92 @@
+{config, pkgs, ... }: 
+
+{
+  imports = 
+  [
+    "./hardware.nix"
+
+    "../../common/modules/school.nix"
+    "../../common/modules/dev.nix"
+
+    "../../common/boot.nix"
+    "../../common/locale.nix"
+    "../../common/printing.nix"
+  ];
+
+  networking = {
+    hostName = "Arrakis"; 
+    networkmanager.enable = true;
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "ch";
+    xkbVariant = "de_nodeadkeys";
+  };
+
+  # Configure console keymap
+  console.keyMap = "sg";
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.giannin = {
+    isNormalUser = true;
+    description = "Giannin";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+      kate
+      _1password
+      _1password-gui
+      jetbrains.idea-ultimate
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ];
+
+  
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
+
+}
