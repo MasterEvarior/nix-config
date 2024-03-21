4dbd20ba059ff8f578062cae3afb94f0ff82fbe6
Giannin <contact@giannin.ch>
2024-03-21 10:13:52 +0100

    Fixed formatting

--- common/modules/school/school.nix
+++ common/modules/school/school.nix
@@ -1,11 +1,9 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ 
-    pkgs.onedrive
-    pkgs.zotero 
-  ];
+  environment.systemPackages = with pkgs; [ pkgs.onedrive pkgs.zotero ];
 
   #to connect to the Eduroam WIFI, it is necessary to install these certificates
-  security.pki.certificateFiles = [ ./DigiCertGlobalRootCA.pem ./DigiCertTLSRSASHA2562020CA1-1.pem];
+  security.pki.certificateFiles =
+    [ ./DigiCertGlobalRootCA.pem ./DigiCertTLSRSASHA2562020CA1-1.pem ];
 }
