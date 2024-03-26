60007deae051fdec056cac8c52d19e83681596f2
Giannin <contact@giannin.ch>
2024-03-26 15:17:19 +0100

    Upgraded to Java 21

--- common/modules/dev/java.nix
+++ common/modules/dev/java.nix
@@ -3,8 +3,12 @@
 {
   environment.systemPackages = with pkgs; [
     pkgs.jetbrains.idea-ultimate
-    pkgs.zulu17
     pkgs.maven
     pkgs.gradle_7
   ];
+
+  programs.java = {
+    package=pkgs.jdk21;
+    enable=true;
+  };
 }
