8327f524d593cd4c7e0ba419458e69cb36648839
Giannin <contact@giannin.ch>
2024-03-26 16:58:51 +0100

    A standard configuration for the OneDrive client is now included

--- common/modules/school/school.nix
+++ common/modules/school/school.nix
@@ -3,6 +3,12 @@
 {
   environment.systemPackages = with pkgs; [ pkgs.onedrive pkgs.zotero ];
 
+
+  environment.etc."config" = {
+    target="onedrive/config";
+    source=./config;
+  };
+
   #to connect to the Eduroam WIFI, it is necessary to install these certificates
   security.pki.certificateFiles =
     [ ./DigiCertGlobalRootCA.pem ./DigiCertTLSRSASHA2562020CA1-1.pem ];
