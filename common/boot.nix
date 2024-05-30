4fea3a7550c88f3735a15405a9054dd67a46b32a
Giannin <contact@giannin.ch>
2024-05-30 09:15:39 +0200

    Added "nodev" option to grub

--- common/boot.nix
+++ common/boot.nix
@@ -4,6 +4,12 @@
   boot.loader = {
     grub.enable = true;
     grub.configurationLimit = 20;
+    
+    # this will tell Grub that it does not need to install itself
+    # because it is a UEFI boot
+    # https://discourse.nixos.org/t/question-about-grub-and-nodev/37867/2
+    grub.device = "nodev";
+
     efi.canTouchEfiVariables = true;
   };
 
