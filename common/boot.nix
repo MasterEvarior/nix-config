d860c2686a287bb75b7d1a74aed266948e45bfa0
Giannin <contact@giannin.ch>
2024-05-30 14:35:38 +0200

    Added theme to Grub

--- common/boot.nix
+++ common/boot.nix
@@ -6,20 +6,20 @@
     grub.configurationLimit = 20;
     grub.efiSupport = true;
 
-    # this will tell Grub that it does not need to install itself
+    # this will tell Grub that there is no need to install itself
     # because it is a UEFI boot
     # https://discourse.nixos.org/t/question-about-grub-and-nodev/37867/2
     grub.device = "nodev";
 
     efi.canTouchEfiVariables = true;
-  };
 
-  boot.grub2-theme = {
-    enable = true;
-    theme = "stylish";
-    footer = true;
+    # https://github.com/vinceliuice/grub2-themes/issues/190
+    grub2-theme = {
+      enable = true;
+      theme = "stylish";
+      footer = true;
+    };
   };
-
   # Updating the kernel to make it (hopefully) compatible with the Lenovo docking stations
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
