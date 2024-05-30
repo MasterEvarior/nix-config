8cbb3df34049476051ebfbc6eee390f850882cb6
Giannin <contact@giannin.ch>
2024-05-30 13:31:02 +0200

    Fixed wrong options

--- common/boot.nix
+++ common/boot.nix
@@ -6,17 +6,18 @@
     grub.configurationLimit = 20;
     grub.efiSupport = true;
 
-    boot.grub2-theme = {
-      enable = true;
-      theme = "stylish";
-      footer = true;
-    };
     # this will tell Grub that it does not need to install itself
     # because it is a UEFI boot
     # https://discourse.nixos.org/t/question-about-grub-and-nodev/37867/2
     grub.device = "nodev";
 
     efi.canTouchEfiVariables = true;
+  };
+
+  boot.grub2-theme = {
+    enable = true;
+    theme = "stylish";
+    footer = true;
   };
 
   # Updating the kernel to make it (hopefully) compatible with the Lenovo docking stations
