8cc27943cbc6826c6b773f4ea33e54a748018b57
Giannin <contact@giannin.ch>
2024-05-30 08:48:46 +0200

    Updated kernel packages to the latest version

--- common/boot.nix
+++ common/boot.nix
@@ -1,7 +1,12 @@
+{pkgs, ...}:
+
 {
   boot.loader = {
     systemd-boot.enable = true;
     systemd-boot.configurationLimit = 60;
     efi.canTouchEfiVariables = true;
   };
+
+  # Updating the kernel to make it (hopefully) compatible with the Lenovo docking stations
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
