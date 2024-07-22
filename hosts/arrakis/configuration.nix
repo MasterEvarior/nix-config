29d35cb4f69001579e7a04aa8a2696c6c985ced0
Giannin <contact@giannin.ch>
2024-07-22 22:10:20 +0200

    Added firefox as a system package

--- hosts/arrakis/configuration.nix
+++ hosts/arrakis/configuration.nix
@@ -84,6 +84,7 @@
     [
       #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       #  wget
+      firefox
     ];
 
   # This value determines the NixOS release from which the default
