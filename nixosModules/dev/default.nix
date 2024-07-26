120845e3578bb648ebc16a5b39353979ddb2e338
Giannin <contact@giannin.ch>
2024-07-26 17:30:33 +0200

    Removed git alias

--- nixosModules/dev/default.nix
+++ nixosModules/dev/default.nix
@@ -18,7 +18,6 @@
   ];
 
   environment.shellAliases = {
-    gyeet = "git reset --hard origin";
     ggraph = "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
   };
 }
