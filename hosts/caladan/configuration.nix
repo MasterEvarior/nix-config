c241a4b205d5a73264bbc7d88b17986839bc491d
Giannin <contact@giannin.ch>
2024-08-03 21:47:28 +0200

    Removed useless comments

--- hosts/caladan/configuration.nix
+++ hosts/caladan/configuration.nix
@@ -15,7 +15,6 @@
     ../../nixosModules/grub2Theme
 
     ../../users/giannin/giannin.nix
-
   ];
 
   # Custom bootloader theming
@@ -56,12 +55,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # List packages installed in system profile. To search, run:
