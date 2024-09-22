16a54f08f1e59b7df6d56e7047e3dfc6d9662f6f
Giannin <contact@giannin.ch>
2024-09-22 15:07:46 +0200

    Enabled auto discovery for printers

--- nixosModules/settings/printing.nix
+++ nixosModules/settings/printing.nix
@@ -1,4 +1,14 @@
 { lib, ... }:
+
 {
   services.printing.enable = lib.mkDefault true;
+
+  # Enable autodiscovery of network printers
+  # https://nixos.wiki/wiki/Printing#Enable_autodiscovery_of_network_printers
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
 }
