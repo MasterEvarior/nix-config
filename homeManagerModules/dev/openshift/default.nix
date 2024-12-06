27a6c5a48ee3fa8895b49aa2cc617932e04d0cb0
Giannin <contact@giannin.ch>
2024-12-06 17:18:23 +0100

    Formatting

--- homeManagerModules/dev/openshift/default.nix
+++ homeManagerModules/dev/openshift/default.nix
@@ -11,7 +11,7 @@
   };
 
   config = lib.mkIf config.homeModules.dev.openshift.enable {
-  
+
     home.packages = with pkgs; [
       ocm
       openshift
--- nixosModules/settings/shebang.nix
+++ nixosModules/settings/shebang.nix
@@ -1,15 +1,9 @@
-{
-  lib,
-  config,
-  ...
-}:
+{ lib, config, ... }:
 
 {
   options.modules.settings.shebang = {
     enable = lib.mkEnableOption "Filesystem for /bin PATH wrapper";
   };
 
-  config = lib.mkIf config.modules.settings.shebang.enable {
-    services.envfs.enable = true;
-  };
+  config = lib.mkIf config.modules.settings.shebang.enable { services.envfs.enable = true; };
 }
