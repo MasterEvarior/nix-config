d5d1947e910fc59cd574fe12232fd56fc22bed88
Giannin <contact@giannin.ch>
2024-12-06 17:13:24 +0100

    Added git alias based on gh client

--- homeManagerModules/applications/github-cli/default.nix
+++ homeManagerModules/applications/github-cli/default.nix
@@ -11,14 +11,18 @@
   };
 
   config = lib.mkIf config.homeModules.applications.github-cli.enable {
-  
-      programs.gh = {
-        enable = true;
 
-        extensions = with pkgs; [
-          gh-dash
-          gh-poi
-        ];
-      };
+    programs.gh = {
+      enable = true;
+
+      extensions = with pkgs; [
+        gh-dash
+        gh-poi
+      ];
+    };
+
+    programs.git.aliases = {
+      clean-branches = "!(gh poi protect main; gh poi protect master; gh poi protect dev; gh poi protect development; gh poi)";
+    };
   };
 }
