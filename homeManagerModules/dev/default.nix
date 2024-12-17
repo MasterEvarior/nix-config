53049c8772371a1f19d44de1660a04f9e7e8eb6f
Giannin <contact@giannin.ch>
2024-12-17 15:37:39 +0100

    Deactivated some default modules

--- homeManagerModules/dev/default.nix
+++ homeManagerModules/dev/default.nix
@@ -17,8 +17,6 @@
   ];
 
   homeModules.dev = {
-    ansible.enable = lib.mkDefault true;
-    golang.enable = lib.mkDefault true;
     js.enable = lib.mkDefault true;
     nix.enable = lib.mkDefault true;
     typst.enable = lib.mkDefault true;
