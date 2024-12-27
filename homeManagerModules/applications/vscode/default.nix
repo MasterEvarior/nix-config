bee9d06857360d98c0e6559586c1e25aeee00938
Giannin <contact@giannin.ch>
2024-12-28 00:16:12 +0100

    Add TOML plugin

--- homeManagerModules/applications/vscode/default.nix
+++ homeManagerModules/applications/vscode/default.nix
@@ -117,6 +117,7 @@
           vscode-extensions.tomoki1207.pdf
           vscode-extensions.ms-azuretools.vscode-docker
           vscode-extensions.ms-vscode.live-server
+          vscode-extensions.tamasfe.even-better-toml
 
           # Catpuccin Theme
           vscode-extensions.catppuccin.catppuccin-vsc
