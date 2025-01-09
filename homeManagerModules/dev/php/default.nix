ba0741b3867c1b5f3b8263e68766ffaca87b185f
Giannin <puzzle@giannin.ch>
2025-01-09 12:07:55 +0100

    Add additional packages and extensions to PHP HM module

--- homeManagerModules/dev/php/default.nix
+++ homeManagerModules/dev/php/default.nix
@@ -12,9 +12,10 @@
 
   config = lib.mkIf config.homeModules.dev.php.enable {
     home.packages = with pkgs; [
-      php84
+      (php84.withExtensions ({ enabled, all }: enabled ++ [ all.tidy ]))
       php84Packages.composer
       symfony-cli
+      ant
     ];
   };
 }
