b2a82c16f50eb9841400af02aea454c14d6edfcf
Giannin <contact@giannin.ch>
2025-01-17 21:34:35 +0100

    Add SSH for homelab

--- homeManagerModules/applications/1password/default.nix
+++ homeManagerModules/applications/1password/default.nix
@@ -25,6 +25,10 @@
     programs.ssh = lib.mkIf config.homeModules.applications."1password".configureSSH {
       enable = true;
       matchBlocks = {
+        "192.168.68.66" = lib.hm.dag.entryBefore [ "Host *" ] {
+          identitiesOnly = true;
+          identityFile = "~/.ssh/public-keys/homelab_1.pub";
+        };
         "185.79.235.161" = lib.hm.dag.entryBefore [ "Host *" ] {
           identitiesOnly = true;
           identityFile = "~/.ssh/public-keys/cloudscale.pub";
