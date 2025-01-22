1ac849d4eb9fd245007d1fea28f8fb2708b7374d
Giannin <puzzle@giannin.ch>
2025-01-22 14:41:35 +0100

    Add Podman as a possibility for work

--- nixosModules/containers/default.nix
+++ nixosModules/containers/default.nix
@@ -7,42 +7,61 @@
 
 {
   options = {
-    modules.containers.enable = lib.mkEnableOption "Containerization with Docker";
-    modules.containers.rootless = lib.mkOption {
-      default = false;
-      example = false;
-      type = lib.types.bool;
-      description = "Wether or not to use Docker rootless. This needs a restart afterwards.";
-    };
-  };
-
-  config = lib.mkIf config.modules.containers.enable {
-    virtualisation.docker = {
-      enable = true;
-      autoPrune.enable = true;
-      autoPrune.dates = "daily";
-      rootless = {
-        enable = config.modules.containers.rootless;
-        setSocketVariable = config.modules.containers.rootless;
+    modules.containers.enable = lib.mkEnableOption "Containerization";
+    modules.containers.docker = {
+      rootless = lib.mkOption {
+        default = false;
+        example = false;
+        type = lib.types.bool;
+        description = "Wether or not to use Docker rootless. This needs a restart afterwards.";
       };
     };
-
-    environment.systemPackages = with pkgs; [
-      docker-sbom
-      docker-buildx
-    ];
-
-    environment.shellAliases = {
-      # docker compose
-      dcu = "docker compose up";
-      dcd = "docker compose down";
-      dcs = "docker compose stop";
-      dcv = "docker compose config --quiet";
-
-      # docker
-      dktop = "docker stats --format ''\"table {{.Container}}	{{.Name}}	{{.CPUPerc}}  {{.MemPerc}}	{{.NetIO}}	{{.BlockIO}}\"";
-      dkprune = "docker container prune --force && docker image prune --force";
-      dkimyeet = "docker rmi -f $(docker images -aq)"; # delete all unused images
+    modules.containers.podman = {
+      enable = lib.mkEnableOption "Wether to enable Podman or not";
     };
   };
+
+  config =
+    let
+      cfg = config.modules.containers;
+    in
+    lib.mkIf config.modules.containers.enable {
+      virtualisation.docker = {
+        enable = true;
+        autoPrune.enable = true;
+        autoPrune.dates = "daily";
+        rootless = {
+          enable = config.modules.containers.docker.rootless;
+          setSocketVariable = config.modules.containers.docker.rootless;
+        };
+      };
+
+      environment.systemPackages =
+        with pkgs;
+        [
+          docker-sbom
+          docker-buildx
+        ]
+        ++ lib.lists.optionals (cfg.podman.enable) [
+          podman-compose
+        ];
+
+      environment.shellAliases = {
+        # docker compose
+        dcu = "docker compose up";
+        dcd = "docker compose down";
+        dcs = "docker compose stop";
+        dcv = "docker compose config --quiet";
+
+        # docker
+        dktop = "docker stats --format ''\"table {{.Container}}	{{.Name}}	{{.CPUPerc}}  {{.MemPerc}}	{{.NetIO}}	{{.BlockIO}}\"";
+        dkprune = "docker container prune --force && docker image prune --force";
+        dkimyeet = "docker rmi -f $(docker images -aq)"; # delete all unused images
+      };
+
+      virtualisation.podman = {
+        enable = cfg.podman.enable;
+        defaultNetwork.settings.dns_enabled = true;
+      };
+    };
 }
