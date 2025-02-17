281d5df657de628b35cfa3aa2fe979d5c21c11f4
Giannin <contact@giannin.ch>
2025-02-17 12:45:22 +0100

    Add snippets for k8s

--- homeManagerModules/dev/kubernetes/default.nix
+++ homeManagerModules/dev/kubernetes/default.nix
@@ -61,6 +61,86 @@
           vscode-extensions.redhat.vscode-yaml # is needed for the k8s extension
           vscode-extensions.ms-kubernetes-tools.vscode-kubernetes-tools
         ];
+
+        additionalSnippets.yaml = {
+          "Create SOPS secret, do not forget to encrypt!" = {
+            prefix = [ "k-sops-secret" ];
+            description = "Create a k8s secret, ready to be decrypted with SOPS";
+            body = [
+              "apiVersion: v1"
+              "kind: Secret"
+              "type: Opaque"
+              "metadata:"
+              "\tname: $1"
+              "\tnamespace: $2"
+              "stringData:"
+              "\t$3"
+            ];
+          };
+          "Create PVC" = {
+            prefix = [ "k-pvc" ];
+            description = "Create a k8s persisten volume claim";
+            body = [
+              "apiVersion: v1"
+              "kind: PersistentVolumeClaim"
+              "metadata:"
+              "\tname: $1"
+              "\tnamespace: $2"
+              "spec:"
+              "\taccessModes:"
+              "\t\t- $3"
+              "\tstorageClassName: longhorn"
+              "\tresources:"
+              "\t\trequests:"
+              "\t\t\tstorage: $4"
+            ];
+          };
+          "Create service" = {
+            prefix = [ "k-service" ];
+            description = "";
+            body = [
+              "apiVersion: v1"
+              "kind: Service"
+              "metadata:"
+              "\tname: $1"
+              "\tnamespace: $2"
+              "\tlabels:"
+              "\t\tapp: $1"
+              "spec:"
+              "\tselector:"
+              "\t\tapp: $1"
+              "\tports:"
+              "\t\t- protocol: \${3|TCP,UDP|}"
+              "\t\t\tport: \${4|80|}"
+              "\t\t\ttargetPort: $5"
+              "\ttype: ClusterIP"
+            ];
+          };
+          "Create deployment" = {
+            prefix = [ "k-deployment" ];
+            description = "Create a k8s deployment";
+            body = [
+              "apiVersion: apps/v1"
+              "kind: Deployment"
+              "metadata:"
+              "\tname: $1"
+              "\tnamespace: $2"
+              "spec:"
+              "\treplicas: 1"
+              "\tselector:"
+              "\t\tmatchLabels:"
+              "\t\t\tapp: $1"
+              "\ttemplate:"
+              "\t\tmetadata:"
+              "\t\t\tlabels:"
+              "\t\t\t\tapp: $1"
+              "\t\t\tspec:"
+              "\t\t\t\tcontainers:"
+              "\t\t\t\t\t- name: $1"
+              "\t\t\t\t\t\timage: $3"
+            ];
+          };
+        };
       };
     };
 }
