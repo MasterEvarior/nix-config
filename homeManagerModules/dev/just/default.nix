9d7000e7f474f5bd2ecb796f2200580bdd50ca27
Giannin <contact@giannin.ch>
2025-04-14 16:50:55 +0200

    Make VSC extensions for Just optional

--- homeManagerModules/dev/just/default.nix
+++ homeManagerModules/dev/just/default.nix
@@ -8,15 +8,24 @@
 {
   options.homeModules.dev.just = {
     enable = lib.mkEnableOption "Just";
+    setupVisualStudioCode = lib.mkEnableOption "Extensions for VSC that help with writting Just files";
   };
 
-  config = lib.mkIf config.homeModules.dev.just.enable {
-    home.packages = with pkgs; [
-      just
-    ];
+  config =
+    let
+      cfg = config.homeModules.dev.just;
+      optionals = lib.optionals;
+    in
+    lib.mkIf config.homeModules.dev.just.enable {
+      home.packages = with pkgs; [
+        just
+      ];
 
-    homeModules.applications.vscode.additionalExtensions = with pkgs; [
-      vscode-extensions.nefrob.vscode-just-syntax
-    ];
-  };
+      homeModules.applications.vscode.additionalExtensions = optionals (cfg.setupVisualStudioCode) (
+        with pkgs;
+        [
+          vscode-extensions.nefrob.vscode-just-syntax
+        ]
+      );
+    };
 }
