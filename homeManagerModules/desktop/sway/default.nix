97080153a9699e5ffd24b11fe10433d50412737a
Giannin <contact@giannin.ch>
2025-05-04 16:07:25 +0200

    Add theme to Sway config

--- homeManagerModules/desktop/sway/default.nix
+++ homeManagerModules/desktop/sway/default.nix
@@ -25,6 +25,34 @@
   config =
     let
       cfg = config.homeModules.desktop.sway;
+      theme = {
+        rosewater = "#f4dbd6";
+        flamingo = "#f0c6c6";
+        pink = "#f5bde6";
+        mauve = "#c6a0f6";
+        red = "#ed8796";
+        maroon = "#ee99a0";
+        peach = "#f5a97f";
+        yellow = "#eed49f";
+        green = "#a6da95";
+        teal = "#8bd5ca";
+        sky = "#91d7e3";
+        sapphire = "#7dc4e4";
+        blue = "#8aadf4";
+        lavender = "#b7bdf8";
+        text = "#cad3f5";
+        subtext1 = "#b8c0e0";
+        subtext0 = "#a5adcb";
+        overlay2 = "#939ab7";
+        overlay1 = "#8087a2";
+        overlay0 = "#6e738d";
+        surface2 = "#5b6078";
+        surface1 = "#494d64";
+        surface0 = "#363a4f";
+        base = "#24273a";
+        mantle = "#1e2030";
+        crust = "#181926";
+      };
     in
     lib.mkIf config.homeModules.desktop.sway.enable {
       assertions = [
@@ -37,6 +65,7 @@
       wayland.windowManager.sway = {
         enable = true;
         checkConfig = cfg.checkConfig;
+        package = null;
         config = {
           terminal = cfg.terminal;
           input = {
@@ -48,6 +77,44 @@
               accel_profile = "flat"; # disable mouse acceleration (enabled by default; to set it manually, use "adaptive" instead of "flat")
               pointer_accel = "0.0"; # set mouse sensitivity (between -1 and 1)
             };
+          };
+          colors = {
+            focused = {
+              background = theme.base;
+              border = theme.lavender;
+              childBorder = theme.lavender;
+              indicator = theme.rosewater;
+              text = theme.text;
+            };
+            focusedInactive = {
+              background = theme.base;
+              border = theme.overlay0;
+              childBorder = theme.overlay0;
+              indicator = theme.rosewater;
+              text = theme.text;
+            };
+            unfocused = {
+              background = theme.base;
+              border = theme.overlay0;
+              childBorder = theme.overlay0;
+              indicator = theme.rosewater;
+              text = theme.text;
+            };
+            urgent = {
+              background = theme.base;
+              border = theme.peach;
+              childBorder = theme.peach;
+              indicator = theme.overlay0;
+              text = theme.peach;
+            };
+            placeholder = {
+              background = theme.base;
+              border = theme.overlay0;
+              childBorder = theme.overlay0;
+              indicator = theme.overlay0;
+              text = theme.text;
+            };
+            background = theme.base;
           };
           startup = [ ];
         };
