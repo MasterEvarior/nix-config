148dfa4e2c1a8f62a405cba77baef53e6feb9cec
Giannin <contact@giannin.ch>
2025-05-12 12:17:26 +0200

    Add more options to Mako config

--- homeManagerModules/desktop/sway/mako.nix
+++ homeManagerModules/desktop/sway/mako.nix
@@ -18,11 +18,30 @@
       type = lib.types.attrs;
       description = "Theme";
     };
+    defaultTimeout = lib.mkOption {
+      default = 15;
+      example = 15;
+      type = lib.types.int;
+      description = "Default timeout in seconds, set to 0 to disable";
+    };
+    ignoreTimout = lib.mkOption {
+      default = true;
+      example = true;
+      type = lib.types.bool;
+      description = "If true, mako will ignore the expire timeout sent by notifications and use the one provided by default-timeout instead.";
+    };
+    maxVisible = lib.mkOption {
+      default = 3;
+      example = 5;
+      type = lib.types.int;
+      description = "Set maximum number of visible notifications. All older notifications will be hidden. If -1, all notifications are visible.";
+    };
   };
 
   config =
     let
-      theme = config.homeModules.desktop.sway.mako.theme;
+      cfg = config.homeModules.desktop.sway.mako;
+      theme = cfg.theme;
     in
     lib.mkIf config.homeModules.desktop.sway.mako.enable {
       home.packages = with pkgs; [
@@ -30,6 +49,12 @@
       ];
 
       home.file.".config/mako/config".text = ''
+        default-timeout=${toString (cfg.defaultTimeout * 1000)}
+        ignore-timeout=${if cfg.ignoreTimout then "1" else "0"}
+        max-visible=${toString cfg.maxVisible}
+
+        border-radius=15
+
         background-color=${theme.base}
         text-color=${theme.text}
         border-color=${theme.rosewater}
