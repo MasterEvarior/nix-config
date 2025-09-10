59610bdf831c1636d285346b8a6786d63aa6333d
Giannin <puzzle@giannin.ch>
2025-09-10 15:37:12 +0200

    Add option to use Bake for Docker Compose builds

--- homeManagerModules/dev/containerization/default.nix
+++ homeManagerModules/dev/containerization/default.nix
@@ -9,23 +9,37 @@
 {
   options.homeModules.dev.containerization = {
     enable = lib.mkEnableOption "Containers";
+    delegateComposeBuilds = lib.mkOption {
+      default = true;
+      example = true;
+      type = lib.types.bool;
+      description = "Wether or not to delegate builds for Docker Compose to Bake for improved performance";
+    };
   };
 
-  config = lib.mkIf config.homeModules.dev.containerization.enable {
-    assertions = [
-      {
-        assertion = osConfig.modules.containers.enable;
-        message = "Containers need to be enabled at OS level for the dev.containerization home manager module to work";
-      }
-    ];
+  config =
+    let
+      cfg = config.homeModules.dev.containerization;
+    in
+    lib.mkIf config.homeModules.dev.containerization.enable {
+      assertions = [
+        {
+          assertion = osConfig.modules.containers.enable;
+          message = "Containers need to be enabled at OS level for the dev.containerization home manager module to work";
+        }
+      ];
 
-    home.packages = with pkgs; [
-      lazydocker
-    ];
+      home.packages = with pkgs; [
+        lazydocker
+      ];
 
-    homeModules.applications.vscode.additionalExtensions = with pkgs; [
-      vscode-extensions.ms-azuretools.vscode-docker
-    ];
+      homeModules.applications.vscode.additionalExtensions = with pkgs; [
+        vscode-extensions.ms-azuretools.vscode-docker
+      ];
 
-  };
+      home.sessionVariables = {
+        COMPOSE_BAKE = (toString cfg.delegateComposeBuilds);
+      };
+
+    };
 }
