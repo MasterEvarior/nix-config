d1597d730843af08ffffa51f7fe2db839e04a452
Giannin <contact@giannin.ch>
2025-10-17 20:18:28 +0200

    Make Clion installation optional

--- homeManagerModules/dev/c/default.nix
+++ homeManagerModules/dev/c/default.nix
@@ -11,19 +11,23 @@
     installCLion = lib.mkEnableOption "Install CLion from Jetbrains";
   };
 
-  config = 
-  let
-    cfg = config.homeModules.dev.c;
-  in
-  lib.mkIf config.homeModules.dev.c.enable {
-    home.packages = with pkgs; [
-      gcc
-      gnumake
-    ] ++ lib.lists.optionals (cfg.installCLion) [
-      jetbrains.clion];
+  config =
+    let
+      cfg = config.homeModules.dev.c;
+    in
+    lib.mkIf config.homeModules.dev.c.enable {
+      home.packages =
+        with pkgs;
+        [
+          gcc
+          gnumake
+        ]
+        ++ lib.lists.optionals (cfg.installCLion) [
+          jetbrains.clion
+        ];
 
-    homeModules.applications.vscode.additionalExtensions = [
-      pkgs.vscode-extensions.ms-vscode.cpptools
-    ];
-  };
+      homeModules.applications.vscode.additionalExtensions = [
+        pkgs.vscode-extensions.ms-vscode.cpptools
+      ];
+    };
 }
