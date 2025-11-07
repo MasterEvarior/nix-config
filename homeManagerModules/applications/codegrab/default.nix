bbfee00705c2ec8a0aae35f61e569e9e34d816f1
Giannin <contact@giannin.ch>
2025-11-07 20:59:12 +0100

    Add alias to configure codegrab

--- homeManagerModules/applications/codegrab/default.nix
+++ homeManagerModules/applications/codegrab/default.nix
@@ -8,11 +8,20 @@
 {
   options.homeModules.applications.codegrab = {
     enable = lib.mkEnableOption "Interactive CLI tool for selecting and bundling code into a single, LLM-ready output file";
+    package = lib.mkPackageOption pkgs-unstable "codegrab" { };
   };
 
-  config = lib.mkIf config.homeModules.applications.codegrab.enable {
-    home.packages = with pkgs-unstable; [
-      codegrab
-    ];
-  };
+  config =
+    let
+      cfg = config.homeModules.applications.codegrab;
+    in
+    lib.mkIf config.homeModules.applications.codegrab.enable {
+      home.packages = [
+        cfg.package
+      ];
+
+      home.shellAliases = {
+        grab = "${lib.getExe cfg.package} --theme catppuccin-macchiato --icons";
+      };
+    };
 }
