d25fa60cf168e77fee445c036eade802ce3a137a
Giannin <contact@giannin.ch>
2025-11-08 21:17:02 +0100

    Add some Maven aliases

--- homeManagerModules/dev/java/default.nix
+++ homeManagerModules/dev/java/default.nix
@@ -8,19 +8,40 @@
 {
   options.homeModules.dev.java = {
     enable = lib.mkEnableOption "Java";
+    mavenPackage = lib.mkPackageOption pkgs "maven" { };
+    gradlePackage = lib.mkPackageOption pkgs "gradle" { };
   };
 
-  config = lib.mkIf config.homeModules.dev.java.enable {
-    home.packages = with pkgs; [
-      jetbrains.idea-ultimate
-      maven
-      gradle
-      gradle-completion
-    ];
+  config =
+    let
+      cfg = config.homeModules.dev.java;
+    in
+    lib.mkIf config.homeModules.dev.java.enable {
+      home.packages = with pkgs; [
+        jetbrains.idea-ultimate
 
-    programs.java = {
-      package = pkgs.jdk23;
-      enable = true;
+        # Maven
+        cfg.mavenPackage
+
+        # Gradle
+        cfg.gradlePackage
+        gradle-completion
+      ];
+
+      home.shellAliases =
+        let
+          mavenBin = lib.getExe cfg.mavenPackage;
+        in
+        {
+          mcv = "${mavenBin} clean verify";
+          mct = "${mavenBin} clean test";
+          mcp = "${mavenBin} clean package";
+          mci = "${mavenBin} clean install";
+        };
+
+      programs.java = {
+        package = pkgs.jdk23;
+        enable = true;
+      };
     };
-  };
 }
