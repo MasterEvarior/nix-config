1f29c23c6ad32d07d404babc9eefab795e138e7d
Giannin <contact@giannin.ch>
2026-01-27 20:50:23 +0100

    Add some more options to OpenCode

--- homeManagerModules/applications/opencode/default.nix
+++ homeManagerModules/applications/opencode/default.nix
@@ -8,11 +8,51 @@
 {
   options.homeModules.applications.opencode = {
     enable = lib.mkEnableOption "Opencode CLI";
-  };
-
-  config = lib.mkIf config.homeModules.applications.opencode.enable {
-    programs.opencode = {
-      enable = true;
+    share = lib.mkOption {
+      default = "disabled";
+      example = "disabled";
+      type = lib.types.enum [
+        "manual"
+        "auto"
+        "disabled"
+      ];
+      description = "See here: https://opencode.ai/docs/config/#sharing";
+    };
+    theme = lib.mkOption {
+      default = "catppuccin-macchiato";
+      example = "catppuccin";
+      type = lib.types.enum [
+        "system"
+        "tokyonight"
+        "everforest"
+        "ayu"
+        "catppuccin"
+        "catppuccin-macchiato"
+        "gruvbox"
+        "kanagawa"
+        "nord"
+        "matrix"
+        "one-dark"
+      ];
+      description = "See here: https://opencode.ai/docs/themes";
     };
   };
+
+  config =
+    let
+      cfg = config.homeModules.applications.opencode;
+    in
+    lib.mkIf config.homeModules.applications.opencode.enable {
+      programs.opencode = {
+        enable = true;
+        settings = {
+          theme = cfg.theme;
+          share = cfg.share;
+          plugin = [
+            "opencode-gemini-auth@latest"
+          ];
+          autoupdate = false;
+        };
+      };
+    };
 }
