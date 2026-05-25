e44e6b8c059d05cb9c4512041108a237f75bfc09
Giannin <contact@giannin.ch>
2026-05-25 18:51:17 +0200

    Remove unused packages

--- homeManagerModules/applications/utils/default.nix
+++ homeManagerModules/applications/utils/default.nix
@@ -12,17 +12,9 @@
 
   config = lib.mkIf config.homeModules.applications.utils.enable {
     home.packages = with pkgs; [
-      # Documents
-      pandoc
-      poppler-utils
-
       # Network
       wget
       curl
     ];
-
-    home.shellAliases = {
-      "pdftopng" = "pdftoppm -singlefile -png -f ";
-    };
   };
 }
