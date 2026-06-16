1361e4becc0d31b0102d896073a1237770657eca
Giannin <contact@giannin.ch>
2026-06-16 09:34:41 +0200

    Remove swaylock background image script

--- homeManagerModules/desktop/sway/swaylock.nix
+++ homeManagerModules/desktop/sway/swaylock.nix
@@ -2,52 +2,27 @@
   lib,
   config,
   pkgs,
-  osConfig,
   ...
 }:
 
 {
-  options.homeModules.desktop.sway.swaylock =
-    let
-      # Extract the serial from something like this: "LG Electronics LG ULTRAWIDE 0x00059AB6" to this: 0x00059AB6
-      extractSerial = output: lib.lists.last (lib.strings.splitString " " output);
-      # Get the correct output name from the JSON, something like DP-3
-      extractOutputName =
-        output:
-        "$(swaymsg -t get_outputs | jq -r '.[] | select(.type == \"output\") | select( .serial | endswith(\"${(extractSerial output)}\")) | .name')";
-      # Extract the image path from something like "./my/image.png fill"
-      extractImagePath = bgValue: builtins.elemAt (lib.strings.splitString " " bgValue) 0;
-      extractBackgrounds =
-        outputs:
-        lib.strings.concatStrings (
-          lib.attrsets.mapAttrsToList (
-            name: value: " --image \"${(extractOutputName name)}\":${extractImagePath (value.bg)}"
-          ) outputs
-        );
-    in
-    {
-      enable = lib.mkEnableOption "Swaylock";
-      theme = lib.mkOption {
-        example = {
-          subtext0 = "#a5adcb";
-          overlay2 = "#939ab7";
-        };
-        type = lib.types.attrs;
-        description = "Theme";
+  options.homeModules.desktop.sway.swaylock = {
+    enable = lib.mkEnableOption "Swaylock";
+    theme = lib.mkOption {
+      example = {
+        subtext0 = "#a5adcb";
+        overlay2 = "#939ab7";
       };
-      backgroundImages = lib.mkOption {
-        default = (extractBackgrounds osConfig.modules.desktop.sway.outputs);
-        example = "./my/image.png";
-        type = lib.types.str;
-        description = "";
-      };
+      type = lib.types.attrs;
+      description = "Theme";
     };
+  };
 
   config =
     let
       cfg = config.homeModules.desktop.sway.swaylock;
       stripHashtag = lib.strings.stringAsChars (x: if x == "#" then "" else x);
-      lockCommand = "${lib.getExe pkgs.swaylock} --show-keyboard-layout --indicator-idle-visible --indicator-caps-lock ${cfg.backgroundImages}";
+      lockCommand = "${lib.getExe pkgs.swaylock} --show-keyboard-layout --indicator-idle-visible --indicator-caps-lock --ignore-empty-password";
     in
     lib.mkIf config.homeModules.desktop.sway.swaylock.enable {
       home.packages = with pkgs; [
